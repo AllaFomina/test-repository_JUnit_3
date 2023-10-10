2e76853fe5e8bb4b019a14811070603d1834f66c
Alla49 <147275808+AllaFomina@users.noreply.github.com>
2023-10-10 21:08:51 +0500

    MainClass_JUnit

---
+++ MainClass.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+public class MainClass {
+
+        private String class_string;
+
+        @Test
+        public void getClassString() {
+                System.out.println("Hello, World!");
+        }
+}
+
+
+
+
+
